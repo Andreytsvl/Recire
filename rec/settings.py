22ce82af1ad9697f91dad71e2d81ee4b814c9b91
unknown <andreytsvl15@gmail.com>
2024-12-23 07:43:00 +0300

    final  requirements

--- rec/settings.py
+++ rec/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8=j5*53aoxnr_8iit!_c^&-m(tw2kss#f$n*-etkvd@g!%3qm&'
+# SECRET_KEY = 'django-insecure-8=j5*53aoxnr_8iit!_c^&-m(tw2kss#f$n*-etkvd@g!%3qm&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'petrov85a.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -122,6 +128,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
