fa33caef744f550de2927740f3ad74da8dd7ed6c
unknown <andreytsvl15@gmail.com>
2024-12-23 08:11:46 +0300

    final  requirements1

--- rec/settings.py
+++ rec/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'petrov85a.pythonanywhere.com',
 ]
-
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -82,10 +84,22 @@ WSGI_APPLICATION = 'rec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petrov85a$default',
+        'USER': 'petrov85a',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'petrov85a.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",'charset': 'utf8mb4',
+        },
     }
 }
 
